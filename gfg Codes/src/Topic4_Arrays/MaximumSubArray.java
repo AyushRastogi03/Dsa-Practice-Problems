4f2575fc8c4d771579edb085a08bad16a7f26766
Ayush Rastogi <arastogi376@gmail.com>
2021-12-08 22:14:21 +0530

    commiting on 8dec

---
+++ gfg Codes/src/Topic4_Arrays/MaximumSubArray.java
@@ -0,0 +1,42 @@
+package Topic4_Arrays;
+
+import java.util.Scanner;
+
+public class MaximumSubArray {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		
+		Scanner sc = new Scanner(System.in);
+		
+		int n = sc.nextInt();
+		
+		int[] arr = new int[n];
+		
+		for(int i=0;i<n;i++) {
+			arr[i] = sc.nextInt();
+			
+		}
+		
+		int res = arr[0];
+		int maxEnd = arr[0];
+    
+		for(int i=1;i<n;i++) {
+			
+			maxEnd = max(maxEnd + arr[i],arr[i]);
+			
+			res = max(res,maxEnd);
+		}
+		
+		System.out.println(res);
+	}
+	
+	public static int max(int a,int b) {
+		if(a>b)
+			return a;
+		else
+	        return b;		
+	
+	}
+
+}
